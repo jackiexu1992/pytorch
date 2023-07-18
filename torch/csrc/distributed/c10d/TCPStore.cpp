e8b6d77b1e846b4b80e02e49963d12193ea52bfc
Xilun Wu <12968408+XilunWu@users.noreply.github.com>
2023-07-18 00:00:57 -0700

    Produce more logs from TCPStore init
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/105350
    
    this diff:
    1. adds debug logs to TCPStore initialization to better capture the "connection reset by peer" error.
    ghstack-source-id: 194891648
    
    Differential Revision: [D47454956](https://our.internmc.facebook.com/intern/diff/D47454956/)

--- torch/csrc/distributed/c10d/TCPStore.cpp
+++ torch/csrc/distributed/c10d/TCPStore.cpp
@@ -1,4 +1,5 @@
 #include <c10/util/irange.h>
+#include <torch/csrc/distributed/c10d/logging.h>
 #include <torch/csrc/distributed/c10d/TCPStore.hpp>
 
 #include <fcntl.h>
@@ -1098,6 +1099,10 @@ TCPStore::TCPStore(std::string host, const TCPStoreOptions& opts)
 
   if (opts.isServer) {
     server_ = detail::TCPServer::start(opts);
+    // server successfully started
+    C10D_DEBUG(
+      "The server has started on port = {}.",
+      server_->port());
 
     addr_.port = server_->port();
   } else {
@@ -1105,12 +1110,16 @@ TCPStore::TCPStore(std::string host, const TCPStoreOptions& opts)
   }
 
   client_ = detail::TCPClient::connect(addr_, opts);
+  // TCP connection established
+  C10D_DEBUG("TCP client connected to host {}:{}", addr_.host, addr_.port);
 
   if (opts.waitWorkers) {
     waitForWorkers();
   }
 
   callbackClient_ = detail::TCPCallbackClient::connect(addr_, opts);
+  // TCP CallbackClient connection established
+  C10D_DEBUG("TCP callback client connected to host {}:{}", addr_.host, addr_.port);
 }
 
 TCPStore::~TCPStore() = default;
